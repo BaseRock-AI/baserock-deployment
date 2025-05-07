700eff9b1c491008d4f9f717f4ead529f5d29cb8
rohit-sapientai <rohit@baserock.ai>
2025-05-07 15:09:47 +0530

    onprem lumber

--- commons/ingress/ingress-cleanup.sh
+++ commons/ingress/ingress-cleanup.sh
@@ -4,3 +4,6 @@ kubectl delete ingressclass nginx -n ingress-nginx
 kubectl get all -n ingress-nginx | grep nginx
 kubectl delete deployment,svc,configmap,ingressclass -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx
 kubectl delete namespace "${INGRESS_NAMESPACE}" 2>/dev/null || true
+
+kubectl delete ingress --all -n "${NAMESPACE}"
+
