bf0d30d804c6ed91ccf58e81b88cccfe26e457ba
bhuvan <bhuvan@sapient.ai>
2025-07-29 22:30:54 +0530

    SM-1428 change for on prem private

--- vars/prod-gcp-vars.sh
+++ vars/prod-gcp-vars.sh
@@ -1,6 +1,6 @@
 # -- gke --#
 export BUCKET_NAME="gs://docker-images-baserock-server"
-export GKE_CLUSTER="on-prem-cluster"
+export GKE_CLUSTER="private-on-prem-1"
 export GKE_REGION="us-central1"
 export PROJECT_ID="production-385606"
 export DOMAIN="app.onprem.baserock.ai"
@@ -14,17 +14,17 @@ export IMAGE_PULL_PASSWORD="$(cat commons/aws/key.json)"
 export IMAGE_PULL_EMAIL="lumber-image-puller@production-385606.iam.gserviceaccount.com"
 
 # -- images --#
-export CLIENT_IMAGE="gcr.io/production-385606/client:0.53.0"
-export UPCASTER_IMAGE="gcr.io/production-385606/upcaster:0.270.0"
-export FLINK_IMAGE="gcr.io/production-385606/flinkjob-rabbitmq-aggregation:0.48.2-feature-concentric-deployment.1"
-export DJANGO_IMAGE="gcr.io/production-385606/le-django-server:0.269.0"
-export LE_SERVICE_IMAGE="gcr.io/production-385606/baserock-backend/learning-engine-service:0.164.0"
-export ADMIN_PORTAL_IMAGE="gcr.io/production-385606/admin-portal-ui:0.12.1-SM-697.1"
+export CLIENT_IMAGE="gcr.io/production-385606/client:0.89.1-release-07-23-2025.1"
+export UPCASTER_IMAGE="gcr.io/production-385606/upcaster:0.341.3-release-07-22-2025.0"
+export FLINK_IMAGE="gcr.io/production-385606/flinkjob-rabbitmq-aggregation:0.58.21-release-07-23-2025.0"
+export DJANGO_IMAGE="gcr.io/production-385606/le-django-server:0.341.3-release-07-22-2025.4"
+export LE_SERVICE_IMAGE="gcr.io/production-385606/baserock-backend/learning-engine-service:0.214.1-release-07-23-2025.1"
+export ADMIN_PORTAL_IMAGE="gcr.io/production-385606/admin-portal-ui:0.15.0"
 export TODO_WEB_APP_IMAGE="gcr.io/production-385606/todo-web-app:0.0.1"
 export TODO_WEB_SERVICE_IMAGE="gcr.io/production-385606/todo-web-service:0.0.1"
 export BACKEND_IMAGE="gcr.io/production-385606/sapient-backend/facade-service"
-export BACKEND_TAG="1.182.0"
-export CELERY_WORKER_IMAGE="gcr.io/production-385606/celery-worker:celery-worker-0.318.0"
+export BACKEND_TAG="1.212.0"
+export CELERY_WORKER_IMAGE="gcr.io/production-385606/celery-worker:0.341.3-release-07-22-2025.1"
 #-- backend-extras--#
 export VALUES_YAML="backend/baserock-backend/values-gcp-prod.yaml"
 
