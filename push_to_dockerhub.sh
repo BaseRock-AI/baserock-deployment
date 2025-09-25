aef8b8d27730a351413428ef509459dd4887a2f9
rohit-sapientai <119845314+rohit-sapientai@users.noreply.github.com>
2025-09-25 12:40:01 +0530

    Updating for aws ingress changes

--- push_to_dockerhub.sh
+++ push_to_dockerhub.sh
@@ -8,29 +8,29 @@ ECR_REGISTRY_NAME="baserock/onprem"
 GCR_REGISTRY="gcr.io/production-385606"
 
 IMAGE_PATHS=(
-#  "gcr.io/production-385606/le-django-server"
-#  "gcr.io/production-385606/upcaster"
-#  "gcr.io/production-385606/baserock-backend/learning-engine-service"
-#  "gcr.io/production-385606/client"
-#  "gcr.io/production-385606/celery-worker"
-#  "gcr.io/production-385606/sapient-backend/facade-service"
-#  "gcr.io/production-385606/admin-portal-ui"
-#  "gcr.io/production-385606/flinkjob-rabbitmq-aggregation"
-#  "gcr.io/production-385606/todo-web-service"
-#  "gcr.io/production-385606/todo-web-app"
-#  "mongo"
-#  "swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/bitnami/postgresql"
-#  "rabbitmq"
-#  "swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/bitnami/redis"
-#  "mongo-express"
-#  "quay.io/jetstack/cert-manager-cainjector"
-#  "quay.io/jetstack/cert-manager-controller"
+  "gcr.io/production-385606/le-django-server"
+  "gcr.io/production-385606/upcaster"
+  "gcr.io/production-385606/baserock-backend/learning-engine-service"
+  "gcr.io/production-385606/client"
+  "gcr.io/production-385606/celery-worker"
+  "gcr.io/production-385606/sapient-backend/facade-service"
+  "gcr.io/production-385606/admin-portal-ui"
+  "gcr.io/production-385606/flinkjob-rabbitmq-aggregation"
+  "gcr.io/production-385606/todo-web-service"
+  "gcr.io/production-385606/todo-web-app"
+  "mongo"
+  "swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/bitnami/postgresql"
+  "rabbitmq"
+  "swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/bitnami/redis"
+  "mongo-express"
+  "quay.io/jetstack/cert-manager-cainjector"
+  "quay.io/jetstack/cert-manager-controller"
   "quay.io/jetstack/cert-manager-webhook"
-#  "quay.io/jetstack/cert-manager-acmesolver"
-#  "quay.io/jetstack/cert-manager-startupapicheck"
-#  "registry.k8s.io/ingress-nginx/controller"
-#  "registry.k8s.io/ingress-nginx/kube-webhook-certgen"
-#  "ghcr.io/apache/flink-kubernetes-operator"
+  "quay.io/jetstack/cert-manager-acmesolver"
+  "quay.io/jetstack/cert-manager-startupapicheck"
+  "registry.k8s.io/ingress-nginx/controller"
+  "registry.k8s.io/ingress-nginx/kube-webhook-certgen"
+  "ghcr.io/apache/flink-kubernetes-operator"
 )
 #   "registry.k8s.io/ingress-nginx/kube-webhook-certgen:v20220916-gd32f8c343@sha256:39c5b2e3310dc4264d638ad28d9d1d96c4cbb2b2dcfb52368fe4e3c63f61e10f"
 # Corresponding tags for each image
@@ -38,29 +38,29 @@ IMAGE_PATHS=(
 
 
 TAGS=(
-#  "0.412.1-release-09-11-2025.4"
-#  "0.412.1-release-09-11-2025.5"
-#  "0.228.1-release-09-11-2025.0"
-#  "0.112.1-release-09-11-2025.0"
-#  "0.412.1-release-09-11-2025.3"
-#  "1.221.0"
-#  "0.12.1-SM-697.1"
-#  "0.58.21-release-07-23-2025.0"
-#  "0.0.2"
-#  "0.0.1"
-#  "8.0.0"
-#  "16.1.0-debian-11-r25"
-#  "4.0.2-management"
-#  "7.2.5-debian-12-r0"
-#  "1.0.2-20-alpine3.19"
-#  "v1.15.3"
-#  "v1.15.3"
+  "0.412.1-release-09-11-2025.4"
+  "0.412.1-release-09-11-2025.5"
+  "0.228.1-release-09-11-2025.0"
+  "0.112.1-release-09-11-2025.0"
+  "0.412.1-release-09-11-2025.3"
+  "1.221.0"
+  "0.12.1-SM-697.1"
+  "0.58.21-release-07-23-2025.0"
+  "0.0.2"
+  "0.0.1"
+  "8.0.0"
+  "16.1.0-debian-11-r25"
+  "4.0.2-management"
+  "7.2.5-debian-12-r0"
+  "1.0.2-20-alpine3.19"
   "v1.15.3"
-#  "v1.15.3"
-#  "v1.15.3"
-#  "v1.6.3"
-#  "v20220916-gd32f8c343"
-#  "ccb10b8"
+  "v1.15.3"
+  "v1.15.3"
+  "v1.15.3"
+  "v1.15.3"
+  "v1.6.3"
+  "v20220916-gd32f8c343"
+  "ccb10b8"
 )
 
 # Matching DockerHub repo names (optional override)
@@ -68,26 +68,26 @@ TAGS=(
 
 
 DOCKERHUB_REPOS=(
-#  "le-django-server"
-#  "upcaster"
-#  "learning-engine-service"
-#  "client"
-#  "celery-worker"
-#  "facade-service"
-#  "admin-portal-ui"
-#  "flinkjob-rabbitmq-aggregation"
-#  "todo-web-service"
-#  "todo-web-app"
-#  "mongo"
-#  "postgresql"
-#  "rabbitmq"
-#  "redis"
-#  "mongo-express"
-#  "cert-manager-cainjector"
-#  "cert-manager-controller"
+  "le-django-server"
+  "upcaster"
+  "learning-engine-service"
+  "client"
+  "celery-worker"
+  "facade-service"
+  "admin-portal-ui"
+  "flinkjob-rabbitmq-aggregation"
+  "todo-web-service"
+  "todo-web-app"
+  "mongo"
+  "postgresql"
+  "rabbitmq"
+  "redis"
+  "mongo-express"
+  "cert-manager-cainjector"
+  "cert-manager-controller"
   "cert-manager-webhook"
-#  "cert-manager-acmesolver"
-#  "cert-manager-startupapicheck"
+  "cert-manager-acmesolver"
+  "cert-manager-startupapicheck"
   "ingress-nginx-controller"
   "kube-webhook-certgen"
   "flink-kubernetes-operator"
@@ -107,7 +107,6 @@ done
 
 export DOCKER_DEFAULT_PLATFORM="linux/amd64"
 docker buildx create --use
-
 
 for i in "${!IMAGE_PATHS[@]}"; do
   IMAGE_PATH="${IMAGE_PATHS[$i]}"
@@ -140,19 +139,12 @@ for i in "${!IMAGE_PATHS[@]}"; do
       "$IMAGE_WITH_TAG"
   fi
 
-  # Tag for DockerHub
-#  docker tag "$IMAGE_WITH_TAG" "$DOCKERHUB_IMAGE:$TAG"
-#  docker tag "$IMAGE_WITH_TAG" "$DOCKERHUB_IMAGE:latest"
-#
   # Push to DockerHub
-   echo "📤 Pushing to DockerHub..."
-
-#   docker buildx imagetools create \
-#         --tag "$GCR_IMAGE:$TAG" \
-#         --tag "$GCR_IMAGE:latest" \
-#         "$IMAGE_WITH_TAG"
-#   docker buildx imagetools create  --tag "$DOCKERHUB_IMAGE:latest" "$IMAGE_WITH_TAG"
-#   docker buildx imagetools create  --tag "$DOCKERHUB_IMAGE:$TAG" "$IMAGE_WITH_TAG"
+  echo "📤 Pushing to DockerHub..."
+  docker buildx imagetools create  \
+    --tag "$DOCKERHUB_IMAGE:latest" \
+    --tag "$DOCKERHUB_IMAGE:$TAG" \
+    "$IMAGE_WITH_TAG"
 #   docker push "$DOCKERHUB_IMAGE:$TAG"
 #   docker push "$DOCKERHUB_IMAGE:latest"
 
@@ -162,10 +154,12 @@ for i in "${!IMAGE_PATHS[@]}"; do
   # Push to ECR
   echo "📤 Pushing to AWS ECR..."
   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-  docker buildx imagetools create  --tag "062853520136.dkr.ecr.us-east-1.amazonaws.com/baserock/onprem/cert-manager-webhook:v1.15.3" "quay.io/jetstack/cert-manager-webhook:v1.15.3"
-#  docker buildx imagetools create  --tag "$ECR_IMAGE:latest" "$IMAGE_WITH_TAG"
-  docker push "$ECR_IMAGE:$TAG"
-  docker push "$ECR_IMAGE:latest"
+  docker buildx imagetools create \
+  --tag "$ECR_IMAGE:latest" \
+  --tag "$ECR_IMAGE:$TAG" \
+  "$IMAGE_WITH_TAG"
+#  docker push "$ECR_IMAGE:$TAG"
+#  docker push "$ECR_IMAGE:latest"
 
   echo "✔ Finished pushing $REPO to both DockerHub and ECR"
 done
